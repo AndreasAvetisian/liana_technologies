b076005d164cdb1a2f328f9dd075e409b0ea0b8d
Andreas Avetisian <t0avan00@students.oamk.fi>
2021-11-18 20:29:06 +0200

    Added Footer Component
    Added main frame for footer component.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,30 @@ import styles from '../styles/Footer.module.scss';
 
 export default function Footer() {
     return (
-        <div className={styles.aaa}>111</div>
+        <div className={styles.footer}>
+            <div className={styles.footerContent}>
+                <div className={styles.footerLeftSide}>
+                    <div className={styles.contactInfo}>
+                        <div>Liana Technologies</div>
+                        <div>Sales and inquiries</div>
+                        <div>Email: sales@lianatech.com</div>
+                        <div>Phone: +358 10 387 7053</div>
+                    </div>
+                    <div className={styles.contactUsButtonDiv}>
+                        <button type="button" className={styles.contactUsButton}>Contact Us</button>
+                    </div>
+                </div>
+                <div className={styles.footerRightSide}>
+                    <div className={styles.CPCNI}>
+                        <div className={styles.components}>Company</div>
+                        <div className={styles.components}>Products</div>
+                        <div className={styles.components}>Contact Us</div>
+                        <div className={styles.components}>News</div>
+                        <div className={styles.components}>Intranet</div>
+                    </div>
+                    <div>button</div>
+                </div>
+            </div>
+        </div>
     );
 }
