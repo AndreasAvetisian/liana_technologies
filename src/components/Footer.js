4a9c5c3352f2d7375bff84ef3e323a0448f4313c
Andreas Avetisian <t0avan00@students.oamk.fi>
2021-11-27 13:45:26 +0200

    Footer
    Start updating footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,21 @@
 import React from "react";
 import styles from '../styles/Footer.module.scss';
+import award_1 from "../pictures/footer/awards/award_1.png";
+import award_2 from "../pictures/footer/awards/award_2.png";
+import award_3 from "../pictures/footer/awards/award_3.png";
+import award_4 from "../pictures/footer/awards/award_4.png";
+import award_5 from "../pictures/footer/awards/award_5.png";
+import award_6 from "../pictures/footer/awards/award_6.png";
+import award_7 from "../pictures/footer/awards/award_7.png";
+import award_8 from "../pictures/footer/awards/award_8.png";
+import award_9 from "../pictures/footer/awards/award_9.png";
+import award_10 from "../pictures/footer/awards/award_10.png";
 
 export default function Footer() {
-
-    var socialNetworkButton = (
-        <div className={styles.buttonStyle}>1</div>
-    );
     return (
         <div className={styles.footer}>
-            <div className={styles.footerContent}>
-                <div className={styles.footerLeftSide}>
-                    <div className={styles.contactInfoBox}>
-                        <div className={styles.title, styles.fontWeight}>Liana Technologies</div>
-                        <div className={styles.contactInfo, styles.fontWeight}>Sales and inquiries</div>
-                        <div className={styles.contactInfo}>Email: sales@lianatech.com</div>
-                        <div className={styles.contactInfo}>Phone: +358 10 387 7053</div>
-                    </div>
-                    <div className={styles.contactUsButtonDiv}>
-                        <button type="button" className={styles.contactUsButton}>Contact Us</button>
-                    </div>
-                </div>
-                <div className={styles.footerRightSide}>
-                    <div className={styles.CPCNI}>
-                        <div className={styles.components}>Company</div>
-                        <div className={styles.components}>Products</div>
-                        <div className={styles.components}>Contact Us</div>
-                        <div className={styles.components}>News</div>
-                        <div className={styles.components}>Intranet</div>
-                    </div>
-                    <div className={styles.socialNetworkButtons}>
-                        <div>{socialNetworkButton}</div>
-                        <div>{socialNetworkButton}</div>
-                        <div>{socialNetworkButton}</div>
-                    </div>
-                </div>
+            <div className={styles.awards}>
+                <img src={award_1} />
             </div>
         </div>
     );
